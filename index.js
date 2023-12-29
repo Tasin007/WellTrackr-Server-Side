9485ebe606f4583cdc2c362d31dc12c9cbadb6b6
Tasin007 <mdtasinalam.mta@gmail.com>
2023-12-29 16:04:38 +0600

    getting users from login and register now working on classes

--- index.js
+++ index.js
@@ -1,92 +1,126 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express()
-const port = process.env.PORT || 5000
 
+const app = express();
+const port = process.env.PORT || 5000;
 
-//middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB connection
 
-//mongo connection
 
 
 const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.cit9nsb.mongodb.net/?retryWrites=true&w=majority`;
-
-
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+  serverApi: ServerApiVersion.v1
 });
 
 async function run() {
-  try {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
 
-    // await client.connect();
+        const userCollection = client.db("WellTrackr").collection("users");
+        const classCollection = client.db("WellTrackr").collection("classes");
 
-    // const taskCollection = client.db("taskManagement").collection("allTasks")
-    const userCollection = client.db("taskManagement").collection("users")
-    app.get("/users", async (req, res) => {
-        const users = await userCollection.find().toArray();
-        res.send(users);
-      });
-    app.post("/users", async (req, res) => {
-        const query = { email: req.body.email };
-        const existingUser = await userCollection.findOne(query);
-        if (existingUser) {
-          res.send({ message: "user already exists", insertedId: null });
-        } else {
-          const newUser = req.body;
-          const result = await userCollection.insertOne(newUser);
+        // User Routes
+        app.get("/api/v1/users", async (req, res) => {
+          const users = await userCollection.find().toArray();
+          res.send(users);
+        });
+    
+        app.get("/api/v1/users/:id", async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const users = await userCollection.findOne(query);
+          res.send(users);
+        });
+    
+        app.post("/api/v1/users", async (req, res) => {
+          const query = { email: req.body.email };
+          console.log(55, req.body.email);
+          const existingUser = await userCollection.findOne(query);
+          if (existingUser) {
+            console.log(58, existingUser);
+            res.send({ message: "user already exists", insertedId: null });
+          } else {
+            const newUser = req.body;
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+          }
+        });
+    
+        app.put("/api/v1/users", async (req, res) => {
+          const query = { email: req.body.email };
+          const existingUser = await userCollection.findOne(query);
+          if (!existingUser) {
+            res.send({ message: "user does not exists", insertedId: null });
+          } else {
+            const result = await userCollection.updateOne(query, {
+              $set: req.body,
+            });
+            res.send(result);
+          }
+        });
+    
+        app.delete("/api/v1/users/:id", async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const result = await userCollection.deleteOne(query);
           res.send(result);
-        }
-      });
+        });
 
-    //   app.get("/tasks", async (req, res) => {
-    //     const tasks = await taskCollection.find().toArray();
-    //     res.send(tasks);
-    //   })
+        // Class Routes
+        app.get("/classes", async (req, res) => {
+            const classes = await classCollection.find().toArray();
+            res.send(classes);
+        });
 
-    //   app.post("/tasks", async (req, res) => {
-    //     const newTask = req.body;
-    //     const result = await taskCollection.insertOne(newTask);
-    //     res.send(result);
-    //         })
-            
-    // app.delete("/tasks/:id", async (req, res) => {
-    //     const id = req.params.id;
-    //     const query = { _id: new ObjectId(id) };
-    //     const result = await taskCollection.deleteOne(query);
-    //     res.send(result);
-    //   });
-    //   app.put("/tasks/:id", async (req, res) => {
-    //     const id = req.params.id;
-    //     const filter = { _id: new ObjectId(id) };
-    //     const updatedDoc = { $set: req.body };
-    //     const result = await taskCollection.updateOne(filter, updatedDoc);
-    //     res.send(result);
-    //   });
+        app.post("/classes", async (req, res) => {
+            const newClass = req.body;
+            const result = await classCollection.insertOne(newClass);
+            res.send(result);
+        });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // await client.close();
-  }
+        // Enroll a user in a class
+        app.post("/enroll", async (req, res) => {
+            const { userId, classId } = req.body;
+            const userObjectId = new ObjectId(userId);
+            const classObjectId = new ObjectId(classId);
+
+            const updateUserResult = await userCollection.updateOne(
+                { _id: userObjectId },
+                { $addToSet: { enrolledClasses: classObjectId } }
+            );
+
+            const updateClassResult = await classCollection.updateOne(
+                { _id: classObjectId },
+                { $inc: { members: 1 } }
+            );
+
+            if (updateUserResult.modifiedCount === 1 && updateClassResult.modifiedCount === 1) {
+                res.status(200).send({ message: "Enrolled successfully" });
+            } else {
+                res.status(400).send({ message: "Unable to enroll" });
+            }
+        });
+
+    } finally {
+        // Uncomment the following line if you want to close the connection after each operation
+        // await client.close();
+    }
 }
+
 run().catch(console.dir);
 
-
-
 app.get('/', (req, res) => {
-  res.send('WellTrackr is available now')
-})
+  res.send('WellTrackr Server is Running');
+});
 
 app.listen(port, () => {
-  console.log(`WellTrackr is running on port ${port}`)
-})
+  console.log(`WellTrackr Server is listening on port ${port}`);
+});
